52d285add65eaa44dd1dff2e9476a2275a024366
lilydoar <lilydoar4@gmail.com>
2023-09-03 16:57:19 -0700

    Add RGB colors

--- src/color.rs
+++ src/color.rs
@@ -2,3 +2,6 @@ type Color = [u8; 4];
 
 pub const BLACK: Color = [0, 0, 0, 255];
 pub const WHITE: Color = [255, 255, 255, 255];
+pub const RED: Color = [255, 0, 0, 255];
+pub const GREEN: Color = [0, 255, 0, 255];
+pub const BLUE: Color = [0, 0, 255, 255];
