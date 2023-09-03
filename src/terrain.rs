fd4d48e3ce2281077f2ebf968ed3832ce0997497
lilydoar <lilydoar4@gmail.com>
2023-09-02 23:11:17 -0700

    Implement thresholding of scalar field

--- src/terrain.rs
+++ src/terrain.rs
@@ -7,6 +7,7 @@ pub struct Terrain {
     pub width: usize,
     pub height: usize,
     pub scalar_field: ScalarField,
+    pub thresholded_field: ScalarField,
 }
 
 impl Terrain {
@@ -32,10 +33,34 @@ impl Terrain {
             }
         }
 
+        let thresholded_field = threshold(&scalar_field, 0.5);
+
         Self {
             width,
             height,
             scalar_field,
+            thresholded_field,
         }
     }
+}
+
+fn threshold(scalar_field: &ScalarField, threshold: f64) -> ScalarField {
+    let mut new_field = Vec::with_capacity(scalar_field.len());
+    for _ in 0..scalar_field.len() {
+        let mut row = Vec::<f64>::with_capacity(scalar_field[0].len());
+        for _ in 0..scalar_field[0].len() {
+            row.push(0.0);
+        }
+        new_field.push(row);
+    }
+
+    for row in 0..scalar_field.len() {
+        for col in 0..scalar_field[0].len() {
+            if scalar_field[row][col] > threshold {
+                new_field[row][col] = 1.0;
+            }
+        }
+    }
+
+    new_field
 }
